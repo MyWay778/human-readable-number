b9bb560fbd15a0e246ad7a73e2d7644d689f09aa
My name <sakla4onakne@mail.ru>
2021-01-20 11:37:57 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
-  
+  if (number === 0) {
+    return 'zero'
+  }
+  number = number.toString().split('')
+  let result = ''
+  let count = 3
+  while (number.length) {
+    if (number.length === 5) {
+      result += getDozen(n.shift())
+      continue
+    }
+
+    if (number.length == 2 && number[0] !== '0'){
+      if (number[0] === '1') {
+        result += getTwoDigitNum(number.join(''))
+        number.splice(0)
+        continue
+      } else {
+        result += getDozen(number.shift())
+        continue
+      }
+    }
+    if(result[result.length-1] !== ' ') {
+      result += ' '
+    }
+    result += getDigit(number.shift())
+
+    if(number.length === 3) {
+      result += ' thousand'
+    }
+    if (number.length === 2 ) {
+      result += ' hundred '
+    }
+  }
+  return result.trim()
+}
+
+function getDigit(n) {
+  const numbers = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'
+  ]
+  return numbers[n]
+}
+function getDozen(n) {
+  const dozens = ['twenty', 'thirty', 'forty', 
+  'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  return dozens[n-2] 
+}
+
+function getTwoDigitNum(n) {
+  const upToTwenty = ['ten', 'eleven', 'twelve','thirteen', 'fourteen', 'fifteen', 
+  'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+    return upToTwenty[n[1]]
 }
